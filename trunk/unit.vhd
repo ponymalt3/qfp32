b46f7fb760224a9ea8e4e16644f8165665130b32
malte <laama@t-online.de>
2018-10-07 17:08:34 +0200

    add trunc function for unit; change unit selection mechanism

--- trunk/unit.vhd
+++ trunk/unit.vhd
@@ -176,6 +176,12 @@ architecture Rtl of qfp_unit is
   signal raw_result : qfp32_raw_t;
   signal result_zero : std_ulogic;
 
+  signal trunc_mask : std_ulogic_vector(28 downto 0);
+  signal sign_mask : std_ulogic;
+  signal result_mov : qfp32_t;
+
+  signal complete_1d : std_ulogic;
+
 begin  -- architecture Rtl
 
   qfp32_add_1: entity work.qfp32_add
@@ -248,24 +254,49 @@ begin  -- architecture Rtl
       raw_i          => raw_result,
       result_o       => result,
       result_zero_o  => result_zero);
-      
-  raw_result <= units_result(i);
+  
+  raw_result <= units_result(j);
 
   process (clk_i, reset_n_i) is
   begin  -- process
     if reset_n_i = '0' then             -- asynchronous reset (active low)
       active_unit <= QFP_UNIT_NONE;
-    elsif clk_i'event and clk_i = '1' then  -- rising clock edge
+      complete_1d <= '0';
+    elsif falling_edge(clk_i) then  -- falling clock edge
       if en_i = '1' then
-        active_unit <= cmd_i.unit;
-        if units_complete(j) = '1' then
+        complete_1d <= units_complete(to_integer(active_unit));
+        
+        if start_i = '1' then
+          if active_unit = QFP_UNIT_NONE or complete_1d = '1' then
+            active_unit <= cmd_i.unit;
+            if units_complete(to_integer(cmd_i.unit)) = '1' then
+              complete_1d <= '1';
+            else
+              complete_1d <= '0';
+            end if;
+          end if;
+        elsif complete_1d = '1' then
           active_unit <= QFP_UNIT_NONE;
         end if;
       end if;
     end if;
   end process;
 
-  units_result(to_integer(QFP_UNIT_NONE)) <= (unsigned(regB_i(28 downto 0)) & X"000000","00000",'0' & unsigned(regB_i(30 downto 29)),regB_i(31));
+  process (cmd_i, regB_i, sign_mask, trunc_mask) is
+  begin  -- process
+    trunc_mask <= (others => '1');
+    sign_mask <= '1';
+
+    if cmd_i.sub_cmd = "01" then
+      trunc_mask(23 downto 0) <= X"FFFFFF" sll (8*(3-to_integer(unsigned(regB_i(30 downto 29)))));
+      sign_mask <= to_ulogic(regB_i(28 downto 24) /= "00000" or unsigned(regB_i(30 downto 29)) /= qfp_x0);
+    end if;
+
+    result_mov <= (unsigned(regB_i(28 downto 0) and trunc_mask),(unsigned(regB_i(30 downto 29)),regB_i(31) and sign_mask));
+                   
+  end process;
+
+  units_result(to_integer(QFP_UNIT_NONE)) <= (result_mov.mant & X"000000","00000",'0' & result_mov.fmt.exp,result_mov.fmt.sign);
   units_ready(to_integer(QFP_UNIT_NONE)) <= '1';
   units_complete(to_integer(QFP_UNIT_NONE)) <= '0';
 
@@ -291,8 +322,8 @@ begin  -- architecture Rtl
   -- misc: 1 cycle, delay 1 cycle
   -- div: 32cycles, delay 31 cycles
   ready_o <= units_ready(j);
-  complete_o <= units_complete(j);
-
+  complete_o <= complete_1d;
+  
   -- only valid for sub
   cmp_gt_o <= not cmp_z and cmp_le;
   cmp_z <= to_ulogic((regA_i xor regB_i) = X"00000000");
